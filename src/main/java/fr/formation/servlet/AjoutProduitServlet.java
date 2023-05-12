e9a5330f1402e3895156a4737164716a7bcac571
Baptiste LEGENDRE <ba.legendre@gmail.com>
2023-05-12 17:18:00 +0200

    piuh

--- src/main/java/fr/formation/servlet/AjoutProduitServlet.java
+++ src/main/java/fr/formation/servlet/AjoutProduitServlet.java
@@ -1,5 +1,36 @@
 package fr.formation.servlet;
 
-public class AjoutProduitServlet {
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/ajoutProduit")
+public class AjoutProduitServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// Affichage du formulaire
+		this.getServletContext()
+			.getRequestDispatcher("/WEB-INF/views/ajoutProduit.jsp")
+			.forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		
+		// Récupérer les paramètres
+//		int id = req.getParameter("id");
+//		req.get
+//		String nom = req.getParameter("nom");
+//		float prix = req.getParameter("prix");
+//		
+		
+	}
+	
+	
 
 }
--- src/main/webapp/WEB-INF/views/ajoutProduit.jsp
+++ src/main/webapp/WEB-INF/views/ajoutProduit.jsp
@@ -8,5 +8,24 @@
 </head>
 <body>
 
+	<form method="POST">
+		<div>
+			<label>id</label>
+			<input type="number" name="id" />
+		</div>
+		
+		<div>
+			<label>nom</label>
+			<input type="text" name="nom" />
+		</div>
+		
+		<div>
+			<label>prix</label>
+			<input type="number" name="prix" />
+		</div>
+		
+		<input type="submit" value="Envoyer" />
+	</form>
+
 </body>
 </html>
