aede60fdd3eb08b5a682010244da735a51d86b2b
marie-emilie <me.deschez@gmail.com>
2023-05-12 15:47:35 +0200

    test

--- src/main/java/fr/formation/servlet/ListeProduitServlet.java
+++ src/main/java/fr/formation/servlet/ListeProduitServlet.java
@@ -1,5 +1,34 @@
 package fr.formation.servlet;
 
-public class ListeProduitServlet {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import fr.formation.model.*;
+import java.util.*;
+
+@WebServlet("/listeProduit")
+public class ListeProduitServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//getParameter pour récupérer un paramètre de requête
+		
+		List<Produit> produits = new ArrayList<>();
+
+		produits.add(new Produit(1, "Volant", 100));
+		produits.add(new Produit(2, "Combinaison", 1000));
+		produits.add(new Produit(3, "Siège", 200));
+		produits.add(new Produit(3, "Roue", 800));
+		
+		//req.setAttribute("produits", produits);
+		
+		req.setAttribute("message", "Shopping en kit");
+		
+		this.getServletContext()	
+		.getRequestDispatcher("/WEB-INF/views/produits.jsp")
+		.forward(req, resp);
+	}
 }
--- src/main/webapp/WEB-INF/views/listeProduit.jsp
+++ src/main/webapp/WEB-INF/views/listeProduit.jsp
@@ -7,6 +7,29 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	<c:if test="${ userSession != null }">
+		<p>Vous avez les droits pour <a href="ajoutProduit">ajouter</a> un produit !</p>
+	</c:if>
+<h1>Produit</h1>
+<table>
+	<thead>
+	<tr>
+	<th>ID</th>
+	<th>Nom</th>
+	<th>Prix</th>
+	</tr>
+	</thead>
+	
+	<tbody>
+	<c:forEach var="produit" items="${ produits }">
+			<tr>
+				<td>${ produit.getId() }</td>
+				<td>${ produit.nom }</td>
+				<td>${ produit.prix }</td>
+			</tr>
+		</c:forEach>
+		</tbody>
+	</table>
+		<p>${ unAttribut }</p>
 </body>
 </html>
