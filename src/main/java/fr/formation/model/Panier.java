de18ed6beda30c2f0105738bb8492b5751030ed5
marie-emilie <me.deschez@gmail.com>
2023-05-12 16:22:27 +0200

    test

--- src/main/java/fr/formation/model/Panier.java
+++ src/main/java/fr/formation/model/Panier.java
@@ -1,5 +1,21 @@
 package fr.formation.model;
 
-public class Panier {
+import java.util.*;
 
+public class Panier {
+	private int id;
+	private List<Produit> produit;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public List<Produit> getProduit() {
+		return produit;
+	}
+	public void setProduit(List<Produit> produit) {
+		this.produit = produit;
+	}
+	
 }
